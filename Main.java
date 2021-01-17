850ce4bfbfa7f282417159531198bfb32c8e6f18
Irina Platonova <crazyangeli@yandex.ru>
2021-01-17 14:40:48 +0300

    add Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 10250;
+        int BonusMiles = cost/20;
+        System.out.println("Начисленные мили - " + BonusMiles);
+    }
+}
