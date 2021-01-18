21101614c64a4f2ea5e8c70bf1f5ccbd496f35c6
Irina Platonova <crazyangeli@yandex.ru>
2021-01-18 19:43:14 +0300

    Reformat code

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 10250;
-        int BonusMiles = cost/20;
-        System.out.println("Начисленные мили - " + BonusMiles);
+        int cost = 10000;
+        int bonusMiles = cost / 20;
+        System.out.println("Начисленные мили - " + bonusMiles);
     }
 }
